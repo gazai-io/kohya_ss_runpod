d66ff69e012f6eb5f85ae81ce2bcc404752f94b3
Jack Tsai <jtsai0606@gmail.com>
2024-08-26 12:25:46 +0800

    update celery to poll from sqs

--- celery_app.py
+++ celery_app.py
@@ -1,9 +1,30 @@
+import os
 from celery import Celery
+from dotenv import load_dotenv
+from kombu.utils.url import safequote
+
+load_dotenv()
+
+SQS_QUEUE_URL = os.environ.get("SQS_QUEUE_URL")
+AWS_ACCESS_KEY_ID = safequote(os.environ.get("AWS_ACCESS_KEY_ID"))
+AWS_SECRET_ACCESS_KEY = safequote(os.environ.get("AWS_SECRET_ACCESS_KEY"))
+AWS_REGION = os.environ.get("AWS_REGION")
+
 
 app = Celery(
     "kohya_ss",
-    broker="redis://localhost:6379/0",  # Adjust broker URL if needed
-    backend="redis://localhost:6379/0",  # Adjust backend URL if needed
+    broker_url=f"sqs://{AWS_ACCESS_KEY_ID}:{AWS_SECRET_ACCESS_KEY}@",
+    broker_transport_options={
+        "region": AWS_REGION,
+        "predefined_queues": {
+            "celery": {
+                "url": f"{SQS_QUEUE_URL}/celery",
+                "access_key_id": AWS_ACCESS_KEY_ID,
+                "secret_access_key": AWS_SECRET_ACCESS_KEY,
+            },
+        },
+    },
+    task_create_missing_queues=False,
     include=["kohya_ss.app.tasks", "kohya_ss.kohya_gui"],
 )
 
